9d4617c515a18f448ca082c6a3cff7a3b1999db6
Emuraye Pius 0 <76441493+oemuraye@users.noreply.github.com>
2022-01-08 21:04:37 +0100

    add product page feature

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,16 @@ app.get("/api/products", (req, res) => {
     res.send(data.products)
 })
 
+app.get('/api/products/:id', (req, res) => {
+    const product = data.products.find((x) => x._id === req.params.id);
+    if (product) { 
+        res.send(product)
+    } else {
+       res.status(404).send({message: 'product Not Found!'}) 
+    }
+})
+
+
 app.listen(5000, () => {
     console.log("Serving at 5000");
 })
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,61 @@
+import { getProduct } from "../api"
+import { parseRequestUrl } from "../utils";
+import Rating from '../components/Rating'
 
 const ProductScreen = {
-    render: () => {
-        return `<div>ProductScreen</div>`
+    render: async () => {
+        const request = parseRequestUrl()
+        const product = await getProduct(request.id)
+        if (product.error) {
+            return `<div>${product.error}</div>`
+        }
+        return `
+            <div class="content">
+                <div class="back-to-result">
+                    <a href="/#/">Back to result</a>
+                </div>
+                <div class="details">
+                    <div class="details-image">
+                        <img src="${product.image}" alt="${product.name}" />
+                    </div>
+                    <div class="details-info">
+                        <ul>
+                            <li>
+                                <h1>${product.name}</h1>
+                            </li>
+                            <li>
+                            ${Rating.render({
+                              value: product.rating,
+                              text: `${product.numReviews} reviews`,
+                            })}
+                            </li>
+                            <li>
+                                Price: <strong>$${product.price}</strong>
+                            </li>
+                            <li>
+                                Description:
+                                <div>
+                                    ${product.description}
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="details-action">
+                        <ul>
+                            <li>
+                                Price: $${product.price}
+                            </li>
+                            <li>
+                                Status : ${product.countInStock > 0 ? `<span class="success">In stock</span>` : `<span class="error">Unavailable</span>` }
+                            </li>
+                            <li>
+                                <button id="add-button" class="primary">Add to Cart</button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        `;
     }
 }
 
