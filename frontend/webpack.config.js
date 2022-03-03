457d3366961f3d4142bee956cf8dcb0eef4899a6
Emuraye Pius 0 <76441493+oemuraye@users.noreply.github.com>
2022-03-03 06:54:01 +0100

    webpack edited

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
   ],
   output: {
     filename: "[name].main.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "/dist"),
     clean: true,
   },
 };
