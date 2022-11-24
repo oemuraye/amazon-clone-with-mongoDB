3caf94da949fb03fa21415a34e4e089ee101bab9
oemuraye <oemuraye@gmail.com>
2022-11-24 14:34:56 +0100

    made adjustment to api url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
-// export const apiUrl = 'https://pius-ecommerce.herokuapp.com'
+export const apiUrl = 'https://pius-ecommerce.herokuapp.com'
 
 // export const apiUrl = 'https://pius-ecommerce.onrender.com'
 
-export const apiUrl = 'http://localhost:5000'
+// export const apiUrl = 'http://localhost:5000'
 
 // export const apiUrl = '/'
